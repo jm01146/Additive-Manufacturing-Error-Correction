b5f4defdb4688068f95e502cb42276bf36302a03
John Marcial <59844100+jm01146@users.noreply.github.com>
2024-09-08 23:19:10 -0400

    Update dataRecord.py

---
+++ dataRecord.py
@@ -0,0 +1,16 @@
+import openpyxl as xl
+import numpy as np
+
+# variable that needs to be collected from the printer
+
+temp = 60 # change the temp of the tempurature of the nozzle with the printing job
+speed = 30 # speed of the print job in mm/s
+
+thickness_cad = 5
+thickness_actual = [4.67, 4.78, 4.58, 4.23, 4.33] 
+
+square_distance_sideA_cad = 
+square_distance_sideA_actual = 
+
+square_distance_sideB_cad = 
+square_distance_sideB_actual = 
